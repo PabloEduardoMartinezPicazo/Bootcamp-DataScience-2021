ab682de96a4d185c525047c3646f464fdfb2fa23
PabloEdaurdoMartinezPicazo <pabloeduardo.martinezpicazo@gmail.com>
2021-04-28 13:31:26 +0200

    teoria y ejercicio 28/04/2021

--- week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
@@ -1,2 +1,2 @@
 class Humano:
-    def__init__
+    def__init__(self, ojos=2,piernas=2,dientes=32,nombre,armadura,nivel,ataque,salud=100,)
---
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
@@ -0,0 +1,2 @@
+class Orco:
+    def__init__(self, ojos=2,piernas=2,dientes=56,nombre,armadura,nivel,ataque,salud=100,)
