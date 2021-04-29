ed65a6710824f68085934f3fedd228e45d850a6e
PabloEdaurdoMartinezPicazo <pabloeduardo.martinezpicazo@gmail.com>
2021-04-29 18:22:33 +0200

    ejercicio orco humano

--- week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
@@ -8,15 +8,16 @@ class humano:
         self.nivel=nivel
         self.ataque=ataque
         self.salud=salud
-        def atacar_humano (self,orco):
-            orco.salud=orco.salud-(self.ataque-orco.armadura)
-            print(orco.salud)
-        def no_vivo_humano(self):
-            if self.salud<=0:
-                return(True)
-            else:
-                return(False)
-        def todos_atributos_humano(self):
-            print(f"ojos:{self.ojos} | piernas: {self.piernas} | dientes:{self.dientes} |\
-                nombre:{self.nombre}  | armadura:{self.armadura} | nivel:{self.nivel} |\
-                    ataque:{self.ataque} | salud:{self.salud}")
+    def atacar_humano(self,orco):
+        orco.salud=orco.salud-(self.ataque-orco.armadura)
+        print(orco.salud)
+        return(orco.salud)
+    def no_vivo_humano(self):
+        if self.salud<=0:
+            return(True)
+        else:
+            return(False)
+    def todos_atributos_humano(self):
+        print(f"ojos:{self.ojos} | piernas: {self.piernas} | dientes:{self.dientes} |\
+            nombre:{self.nombre}  | armadura:{self.armadura} | nivel:{self.nivel} |\
+                ataque:{self.ataque} | salud:{self.salud}")
--- week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
@@ -8,15 +8,16 @@ class orco:
         self.nivel = nivel
         self.ataque = ataque
         self.salud = salud
-        def atacar_orco (self,humano):
-            humano.salud = humano.salud-(self.ataque-humano.armadura)
-            print(humano.salud)
-        def no_vivo(self):
-            if self.salud<= 0:
-                return(True)
-            else:
-                return(False)
-        def todos_atributos(self):
-            print(f"ojos:{self.ojos} | piernas: {self.piernas} | dientes:{self.dientes} |\
-                nombre:{self.nombre}  | armadura:{self.armadura} | nivel:{self.nivel} |\
-                    ataque:{self.ataque} | salud:{self.salud}")
+    def atacar_orco(self,humano):
+        humano.salud = humano.salud-(self.ataque-humano.armadura)
+        print(humano.salud)
+        return(humano.salud)
+    def no_vivo(self):
+        if self.salud<= 0:
+            return(True)
+        else:
+            return(False)
+    def todos_atributos(self):
+        print(f"ojos:{self.ojos} | piernas: {self.piernas} | dientes:{self.dientes} |\
+            nombre:{self.nombre}  | armadura:{self.armadura} | nivel:{self.nivel} |\
+                ataque:{self.ataque} | salud:{self.salud}")
